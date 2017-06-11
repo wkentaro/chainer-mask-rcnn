f80a4a999e0dc421514865613bd5fe9f1818f591
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-11 21:07:52 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='mask_rcnn',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/mask-rcnn',
+    license='MIT',
+)
