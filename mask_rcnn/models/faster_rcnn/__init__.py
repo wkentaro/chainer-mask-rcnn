02ae1c6e58dd3c8e4531543738799d6aeacf8b8f
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-09-07 01:38:46 +0900

    Add faster_rcnn model for reference

---
+++ mask_rcnn/models/faster_rcnn/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from .faster_rcnn import FasterRCNN
+from .faster_rcnn_train_chain import FasterRCNNTrainChain
+from .faster_rcnn_vgg import FasterRCNNVGG16
+from .faster_rcnn_vgg import VGG16RoIHead
