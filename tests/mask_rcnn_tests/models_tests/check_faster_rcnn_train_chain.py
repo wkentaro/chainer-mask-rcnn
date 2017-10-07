75020d2620f5eb5748f5d94fa86a39993e4d77d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-07 17:54:51 +0900

    Add check_faster_rcnn_train_chain.py

--- tests/mask_rcnn_tests/models_tests/check_faster_rcnn_train_chain.py
+++ tests/mask_rcnn_tests/models_tests/check_faster_rcnn_train_chain.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 
-import pprint
+from __future__ import print_function
 
 import chainer
 from chainer import cuda
-import fcn
+import chainercv
 import numpy as np
 
 import mask_rcnn
 
 
 gpu = 0
-
-if gpu >= 0:
-    cuda.get_device(gpu).use()
-
-chainer.config.train = True
-chainer.config.enable_backprop = True
+cuda.get_device(gpu).use()
 
 frcnn = mask_rcnn.models.faster_rcnn.FasterRCNNVGG16(
     n_fg_class=20, pretrained_model='imagenet')
-# frcnn = mask_rcnn.models.faster_rcnn.FasterRCNNVGG16(pretrained_model='voc07')
+# frcnn = mask_rcnn.models.faster_rcnn.FasterRCNNVGG16(
+#    pretrained_model='voc07')
 model = mask_rcnn.models.faster_rcnn.FasterRCNNTrainChain(frcnn)
 if gpu >= 0:
     model.to_gpu()
@@ -29,42 +25,59 @@ optimizer = chainer.optimizers.MomentumSGD(lr=1e-3, momentum=0.9)
 optimizer.setup(model)
 optimizer.add_hook(chainer.optimizer.WeightDecay(rate=0.0005))
 
-dataset_ins = mask_rcnn.datasets.VOC2012InstanceSeg(split='train')
-dataset = mask_rcnn.datasets.MaskRcnnDataset(dataset_ins)
-img, bboxes, labels, _, scale = dataset[0]
-img_org = img.copy().transpose(2, 0, 1)
-print(img.shape)
-print(bboxes.shape)
-print(labels.shape)
-print(scale)
+if True:
+    dataset = chainercv.datasets.VOCDetectionDataset(
+        split='train', year='2012', use_difficult=True)
+    img, bbox, label = dataset.get_example(0)
+else:
+    dataset_ins = mask_rcnn.datasets.VOC2012InstanceSeg(split='train')
+    dataset = mask_rcnn.datasets.MaskRcnnDataset(dataset_ins)
+    img, bbox, label, _, _ = dataset[0]
+    img = img.transpose(2, 0, 1)
+    label -= 1
 
-img, = fcn.datasets.transform_lsvrc2012_vgg16((img,))
-img = np.asarray([img])
-bboxes = np.asarray([bboxes])
-labels = np.asarray([labels])
-scale = np.asarray([scale])
+img_org = img.copy()
+
+_, H, W = img.shape
+img = model.faster_rcnn.prepare(img)
+_, o_H, o_W = img.shape
+scale = 1. * o_H / H
+bbox = chainercv.transforms.resize_bbox(bbox, (H, W), (o_H, o_W))
+
+imgs = np.asarray([img])
+bboxes = np.asarray([bbox])
+labels = np.asarray([label])
+scale = np.array([scale], dtype=np.float32)
 if gpu >= 0:
-    img = cuda.to_gpu(img)
+    imgs = cuda.to_gpu(imgs)
     bboxes = cuda.to_gpu(bboxes)
     labels = cuda.to_gpu(labels)
     scale = cuda.to_gpu(scale)
 
-reporter = chainer.Reporter()
-reporter.add_observer('main', model)
-reporter.add_observers('main', model.namedlinks(skipself=True))
+for i in xrange(50):
+    if i % 10 == 0:
+        bboxes_pred, labels_pred, scores_pred = \
+            model.faster_rcnn.predict([img_org.copy()])
 
-for i in xrange(10000):
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        print(bboxes * scale)
+        print(labels)
+        print('----------------------------------------------------------')
+        print(bboxes_pred)
+        print(labels_pred)
+        print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
+        # bbox_pred = bboxes_pred[0]
+        # bbox_pred = bbox_pred[:, [1, 0, 3, 2]]
+        # label_pred = labels_pred[0]
+        # viz = img_org.transpose(1, 2, 0).astype(np.uint8)
+        # viz = mask_rcnn.utils.draw_instance_boxes(
+        #     viz, bbox_pred, label_pred, n_class=20, bg_class=-1)
+        # cv2.imshow('viz', viz[:, :, ::-1])
+        # cv2.waitKey(0)
+
     model.zerograds()
-    observation = {}
-    with reporter.scope(observation):
-        loss = model(img, bboxes, labels, scale)
-    print(i)
-    pprint.pprint(observation)
+    loss = model(imgs, bboxes, labels, scale)
+    print('[%02d] Loss: %s' % (i, loss))
     loss.backward()
     optimizer.update()
-
-    if i % 10 == 0:
-        print(bboxes, labels)
-        bboxes_pred, labels_pred, scores_pred = \
-            model.faster_rcnn.predict([img_org])
-        print(bboxes_pred, labels_pred)
