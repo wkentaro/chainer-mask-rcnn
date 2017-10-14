cc934e6a14f192cca7011effaeade1765cde65fa
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-15 07:06:14 +0900

    Remove faster_rcnn

--- mask_rcnn/models/__init__.py
+++ mask_rcnn/models/__init__.py
@@ -1,6 +1,4 @@
 # flake8: noqa
-from .faster_rcnn import FasterRCNNVGG16
-from .faster_rcnn import FasterRCNNTrainChain
 from .mask_rcnn import MaskRCNN
 from .mask_rcnn import MaskRCNNVGG16
 from .mask_rcnn import MaskRCNNTrainChain
--- mask_rcnn/models/faster_rcnn/__init__.py
+++
@@ -1,4 +0,0 @@
-from .faster_rcnn import FasterRCNN  # NOQA
-from .faster_rcnn_train_chain import FasterRCNNTrainChain  # NOQA
-from .faster_rcnn_vgg import FasterRCNNVGG16  # NOQA
-from .faster_rcnn_vgg import VGG16RoIHead  # NOQA
