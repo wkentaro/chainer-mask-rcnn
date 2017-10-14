d62fa16b288ce20427cc3f84d5340a3f31c2fae9
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-15 07:05:02 +0900

    Add evaluation code

---
+++ mask_rcnn/utils/evaluations.py
@@ -0,0 +1,165 @@
+from __future__ import division
+
+from collections import defaultdict
+import itertools
+
+import numpy as np
+import six
+
+from .geometry import get_mask_overlap
+
+
+def mask_iou(mask_a, mask_b):
+    if mask_a.shape[1:] != mask_b.shape[1:]:
+        raise ValueError
+
+    I = len(mask_a)
+    J = len(mask_b)
+    iou = np.zeros((I, J), dtype=np.float64)
+    for i, ma in enumerate(mask_a):
+        for j, mb in enumerate(mask_b):
+            ov = get_mask_overlap(ma, mb)
+            iou[i, j] = ov
+    return iou
+
+
+# Original work:
+# https://github.com/chainer/chainercv/blob/master/chainercv/evaluations/eval_detection_voc.py  # NOQA
+def calc_instseg_voc_prec_rec(
+        pred_masks, pred_labels, pred_scores,
+        gt_masks, gt_labels,
+        gt_difficults=None,
+        iou_thresh=0.5):
+    """Calculate precision and recall based on evaluation code of PASCAL VOC.
+
+    This function calculates precision and recall of
+    predicted bounding boxes obtained from a dataset which has :math:`N`
+    images.
+    The code is based on the evaluation code used in PASCAL VOC Challenge.
+
+    Args:
+        pred_labels (iterable of numpy.ndarray): An iterable of labels.
+            Similar to :obj:`pred_bboxes`, its index corresponds to an
+            index for the base dataset. Its length is :math:`N`.
+        pred_scores (iterable of numpy.ndarray): An iterable of confidence
+            scores for predicted bounding boxes. Similar to :obj:`pred_bboxes`,
+            its index corresponds to an index for the base dataset.
+            Its length is :math:`N`.
+        gt_labels (iterable of numpy.ndarray): An iterable of ground truth
+            labels which are organized similarly to :obj:`gt_bboxes`.
+        gt_difficults (iterable of numpy.ndarray): An iterable of boolean
+            arrays which is organized similarly to :obj:`gt_bboxes`.
+            This tells whether the
+            corresponding ground truth bounding box is difficult or not.
+            By default, this is :obj:`None`. In that case, this function
+            considers all bounding boxes to be not difficult.
+        iou_thresh (float): A prediction is correct if its Intersection over
+            Union with the ground truth is above this value..
+
+    Returns:
+        tuple of two lists:
+        This function returns two lists: :obj:`prec` and :obj:`rec`.
+
+        * :obj:`prec`: A list of arrays. :obj:`prec[l]` is precision \
+            for class :math:`l`. If class :math:`l` does not exist in \
+            either :obj:`pred_labels` or :obj:`gt_labels`, :obj:`prec[l]` is \
+            set to :obj:`None`.
+
+        * :obj:`rec`: A list of arrays. :obj:`rec[l]` is recall \
+            for class :math:`l`. If class :math:`l` that is not marked as \
+            difficult does not exist in \
+            :obj:`gt_labels`, :obj:`rec[l]` is \
+            set to :obj:`None`.
+    """
+    pred_masks = iter(pred_masks)
+    pred_labels = iter(pred_labels)
+    pred_scores = iter(pred_scores)
+    gt_masks = iter(gt_masks)
+    gt_labels = iter(gt_labels)
+    if gt_difficults is None:
+        gt_difficults = itertools.repeat(None)
+    else:
+        gt_difficults = iter(gt_difficults)
+
+    n_pos = defaultdict(int)
+    score = defaultdict(list)
+    match = defaultdict(list)
+
+    for pred_mask, pred_label, pred_score, gt_mask, gt_label, gt_difficult in \
+            six.moves.zip(
+                pred_masks, pred_labels, pred_scores,
+                gt_masks, gt_labels, gt_difficults):
+
+        if gt_difficult is None:
+            gt_difficult = np.zeros(gt_mask.shape[0], dtype=bool)
+
+        for l in np.unique(np.concatenate((pred_label, gt_label)).astype(int)):
+            pred_keep_l = pred_label == l
+            pred_mask_l = pred_mask[pred_keep_l]
+            pred_score_l = pred_score[pred_keep_l]
+            # sort by score
+            order = pred_score_l.argsort()[::-1]
+            pred_mask_l = pred_mask_l[order]
+            pred_score_l = pred_score_l[order]
+
+            gt_keep_l = gt_label == l
+            gt_mask_l = gt_mask[gt_keep_l]
+            gt_difficult_l = gt_difficult[gt_keep_l]
+
+            n_pos[l] += np.logical_not(gt_difficult_l).sum()
+            score[l].extend(pred_score_l)
+
+            if len(pred_mask_l) == 0:
+                continue
+            if len(gt_mask_l) == 0:
+                match[l].extend((0,) * pred_mask_l.shape[0])
+                continue
+
+            iou = mask_iou(pred_mask_l, gt_mask_l)
+            gt_index = iou.argmax(axis=1)
+            # set -1 if there is no matching ground truth
+            gt_index[iou.max(axis=1) < iou_thresh] = -1
+            del iou
+
+            selec = np.zeros(gt_mask_l.shape[0], dtype=bool)
+            for gt_idx in gt_index:
+                if gt_idx >= 0:
+                    if gt_difficult_l[gt_idx]:
+                        match[l].append(-1)
+                    else:
+                        if not selec[gt_idx]:
+                            match[l].append(1)
+                        else:
+                            match[l].append(0)
+                    selec[gt_idx] = True
+                else:
+                    match[l].append(0)
+
+    for iter_ in (
+            pred_masks, pred_labels, pred_scores,
+            gt_masks, gt_labels, gt_difficults):
+        if next(iter_, None) is not None:
+            raise ValueError('Length of input iterables need to be same.')
+
+    n_fg_class = max(n_pos.keys()) + 1
+    prec = [None] * n_fg_class
+    rec = [None] * n_fg_class
+
+    for l in n_pos.keys():
+        score_l = np.array(score[l])
+        match_l = np.array(match[l], dtype=np.int8)
+
+        order = score_l.argsort()[::-1]
+        match_l = match_l[order]
+
+        tp = np.cumsum(match_l == 1)
+        fp = np.cumsum(match_l == 0)
+
+        # If an element of fp + tp is 0,
+        # the corresponding element of prec[l] is nan.
+        prec[l] = tp / (fp + tp)
+        # If n_pos[l] is 0, rec[l] is None.
+        if n_pos[l] > 0:
+            rec[l] = tp / n_pos[l]
+
+    return prec, rec
---
+++ tests/mask_rcnn_tests/utils_tests/evaluations_tests/test_calc_instseg_voc_prec_rec.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+
+import chainercv
+import cv2
+import numpy as np
+
+import mask_rcnn
+from mask_rcnn.utils.evaluations import calc_instseg_voc_prec_rec
+
+
+def check_calc_instseg_voc_prec_rec():
+    dataset = mask_rcnn.datasets.VOC2012InstanceSeg('train')
+    img, lbl_cls_true, lbl_ins_true = dataset.get_example(0)
+
+    gt_label, gt_bbox, gt_mask = mask_rcnn.utils.label2instance_boxes(
+        lbl_ins_true, lbl_cls_true, return_masks=True)
+
+    pred_mask = []
+    for m in gt_mask:
+        H, W = m.shape[:2]
+        m = m.astype(np.float32)
+        m = cv2.resize(m, None, None, fx=0.09, fy=0.09)
+        m = cv2.resize(m, (W, H))
+        m = m >= 0.5
+        pred_mask.append(m)
+    pred_mask = np.asarray(pred_mask)
+
+    pred_label = gt_label
+    pred_score = np.random.uniform(0.6, 0.99, (len(pred_mask),))
+
+    lbl_ins_pred, lbl_cls_pred = mask_rcnn.utils.instance_boxes2label(
+        gt_label, gt_bbox, pred_mask)
+
+    viz_true = mask_rcnn.utils.visualize_instance_segmentation(
+        lbl_ins_true, lbl_cls_true, img, dataset.class_names)
+    viz_pred = mask_rcnn.utils.visualize_instance_segmentation(
+        lbl_ins_pred, lbl_cls_pred, img, dataset.class_names)
+    viz = np.vstack([viz_true, viz_pred])
+
+    gt_label -= 1  # background: 0 -> -1
+    gt_masks = [gt_mask]
+    gt_labels = [gt_label]
+
+    pred_label = gt_label
+    pred_masks = [pred_mask]
+    pred_labels = [pred_label]
+    pred_scores = [pred_score]
+
+    prec, rec = calc_instseg_voc_prec_rec(
+        pred_masks, pred_labels, pred_scores, gt_masks, gt_labels)
+
+    ap = chainercv.evaluations.calc_detection_voc_ap(prec, rec)
+    for cls_id, cls_name in enumerate(dataset.class_names[1:]):
+        if cls_id >= len(ap):
+            cls_ap = None
+        else:
+            cls_ap = ap[cls_id]
+        print('ap/%s:' % cls_name, cls_ap)
+    print('map:', np.nanmean(ap))
+
+    cv2.imshow(__file__, viz[:, :, ::-1])
+    cv2.waitKey(0)
+
+
+check_calc_instseg_voc_prec_rec()
