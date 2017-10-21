5adf07cdea12e353f65f1cf761b8954c92b635b1
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-21 19:37:12 +0900

    Fixed roi_align_2d

--- mask_rcnn/models/mask_rcnn_resnet.py
+++ mask_rcnn/models/mask_rcnn_resnet.py
@@ -269,10 +269,7 @@ def _roi_align_2d_yx(x, indices_and_rois, outh, outw, spatial_scale,
                      roi_align=True):
     xy_indices_and_rois = indices_and_rois[:, [0, 2, 1, 4, 3]]
     if roi_align:
-        # FIXME
-        # pool = F.roi_align_2d(
-        #     x, xy_indices_and_rois, outh, outw, spatial_scale)
-        pool = functions.roi_align_2d(
+        pool = F.roi_align_2d(
             x, xy_indices_and_rois, outh, outw, spatial_scale)
     else:
         pool = F.roi_pooling_2d(
