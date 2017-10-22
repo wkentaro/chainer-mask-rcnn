304104f5d887f998a8102eeb236606ea279a5c60
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-22 21:09:53 +0900

    Add crop_and_resize

--- mask_rcnn/functions/__init__.py
+++ mask_rcnn/functions/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
+from .roi_align_2d import crop_and_resize
 from .roi_align_2d import roi_align_2d
 from .roi_align_2d import ROIAlign2D
--- mask_rcnn/functions/roi_align_2d.py
+++ mask_rcnn/functions/roi_align_2d.py
@@ -1,8 +1,12 @@
+import collections
+
 import numpy
 import six
 
+import chainer
 from chainer import cuda
 from chainer import function
+from chainer import functions
 from chainer.utils import type_check
 
 
@@ -502,3 +506,32 @@ def roi_align_2d(x, rois, outh, outw, spatial_scale):
 
     """
     return ROIAlign2D(outh, outw, spatial_scale)(x, rois)
+
+
+def crop_and_resize(bottom_data, bottom_rois, outh, outw, spatial_scale):
+    if isinstance(bottom_rois, chainer.Variable):
+        bottom_rois = bottom_rois.data
+    bottom_rois = cuda.to_cpu(bottom_rois)
+
+    B, C, H, W = bottom_data.shape
+    N = bottom_rois.shape[0]
+
+    ys = collections.defaultdict(list)
+    for i_roi in six.moves.range(N):
+        i_batch, x1, y1, x2, y2 = bottom_rois[i_roi].tolist()
+        i_batch = int(i_batch)
+        x1 = int(spatial_scale * x1)
+        x2 = max(int(spatial_scale * x2), x1 + 1)
+        y1 = int(spatial_scale * y1)
+        y2 = max(int(spatial_scale * y2), y1 + 1)
+        x_roi = bottom_data[i_batch][:, y1:y2, x1:x2]
+        x_roi = x_roi[None, :, :, :]  # N, C, H, W
+        y = functions.resize_images(x_roi, (outh, outw))
+        ys[i_batch].append(y)
+
+    yss = []
+    for i_batch in six.moves.range(B):
+        ys = functions.vstack(ys[i_batch])
+        yss.append(ys)
+    yss = functions.concat(yss, axis=0)
+    return yss
