80f48b0999d0d6ab79de60ff518bb198495d1742
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-23 05:17:49 +0900

    Fix rpn/train.py

--- examples/rpn/train.py
+++ examples/rpn/train.py
@@ -15,72 +15,35 @@ from chainer.datasets import TransformDataset
 from chainer import training
 from chainer.training import extensions
 from chainercv.datasets import voc_bbox_label_names
-from chainercv.links import FasterRCNNResNet101
-from chainercv.links import FasterRCNNResNet50
 from chainercv.links import FasterRCNNVGG16
-from chainercv import transforms
 import numpy as np
 
 import mask_rcnn as mrcnn
 
-
-class Transform(object):
-
-    def __init__(self, faster_rcnn, augmentation=True):
-        self.faster_rcnn = faster_rcnn
-        self._augmentation = augmentation
-
-    def __call__(self, in_data):
-        img, bbox, label = in_data
-        _, H, W = img.shape
-        img = self.faster_rcnn.prepare(img)
-        _, o_H, o_W = img.shape
-        scale = o_H / H
-        bbox = transforms.resize_bbox(bbox, (H, W), (o_H, o_W))
-
-        if self._augmentation:
-            # horizontally flip
-            img, params = transforms.random_flip(
-                img, x_random=True, return_param=True)
-            bbox = transforms.flip_bbox(
-                bbox, (o_H, o_W), x_flip=params['x_flip'])
-
-        return img, bbox, label, scale
-
-
-class FasterRcnnDataset(chainer.dataset.DatasetMixin):
-
-    def __init__(self, instance_dataset):
-        self._instance_dataset = instance_dataset
-
-    def __len__(self):
-        return len(self._instance_dataset)
-
-    def get_example(self, i):
-        img, lbl_cls, lbl_ins = self._instance_dataset.get_example(i)
-        img = img.transpose(2, 0, 1)
-        labels, bboxes, _ = mrcnn.utils.label2instance_boxes(
-            lbl_ins, lbl_cls, return_masks=True)
-        labels = labels.astype(np.int32, copy=False)
-        labels -= 1  # background: 0 -> -1
-        bboxes = bboxes.astype(np.float32, copy=False)
-        return img, bboxes, labels
-
-
-here = osp.dirname(osp.abspath(__file__))
+here = osp.dirname(osp.abspath(__file__))  # NOQA
+sys.path.insert(0, osp.join(here, '../faster_rcnn'))  # NOQA
+from train import FasterRCNNDataset
+from train import FasterRCNNTransform
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # training parameters
     parser.add_argument('--model', choices=('vgg16', 'resnet50', 'resnet101'),
-                        help='The model to use', default='resnet50')
-    parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--lr', '-l', type=float, default=1e-3)
-    parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--step_size', '-ss', type=int, default=50000)
-    parser.add_argument('--iteration', '-i', type=int, default=70000)
-    parser.add_argument('--weight_decay', type=float, default=0.0005)
+                        help='The model to train.', default='resnet50')
+    parser.add_argument('--lr', '-l', type=float, default=1e-3,
+                        help='Learning rate.')
+    parser.add_argument('--seed', '-s', type=int, default=0,
+                        help='Random seed.')
+    parser.add_argument('--step_size', '-ss', type=int, default=50000,
+                        help='Step size of iterations.')
+    parser.add_argument('--iteration', '-i', type=int, default=70000,
+                        help='Iteration size.')
+    parser.add_argument('--weight_decay', type=float, default=0.0005,
+                        help='Weight decay.')
+    # other parameters
+    parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU id.')
     args = parser.parse_args()
 
     args.timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -96,28 +59,24 @@ def main():
             'timestamp={timestamp}',
         ]).format(**args.__dict__)
     )
-
     pprint.pprint(args.__dict__)
 
     np.random.seed(args.seed)
 
-    train_data = FasterRcnnDataset(
-        mrcnn.datasets.VOC2012InstanceSeg(split='train'))
-    test_data = FasterRcnnDataset(
-        mrcnn.datasets.VOC2012InstanceSeg(split='val'))
-
+    # Model / Optimizer
+    # -------------------------------------------------------------------------
     if args.model == 'vgg16':
         faster_rcnn = FasterRCNNVGG16(n_fg_class=len(voc_bbox_label_names),
                                       pretrained_model='imagenet',
                                       min_size=800, max_size=9999)
     elif args.model == 'resnet50':
-        faster_rcnn = FasterRCNNResNet50(n_fg_class=len(voc_bbox_label_names),
-                                         pretrained_model='imagenet',
-                                         min_size=800, max_size=9999)
+        faster_rcnn = mrcnn.models.FasterRCNNResNet(
+            n_layers=50, n_fg_class=len(voc_bbox_label_names),
+            pretrained_model='imagenet', min_size=800, max_size=9999)
     elif args.model == 'resnet101':
-        faster_rcnn = FasterRCNNResNet101(n_fg_class=len(voc_bbox_label_names),
-                                          pretrained_model='imagenet',
-                                          min_size=800, max_size=9999)
+        faster_rcnn = mrcnn.models.FasterRCNNResNet(
+            n_layers=101, n_fg_class=len(voc_bbox_label_names),
+            pretrained_model='imagenet', min_size=800, max_size=9999)
     else:
         raise ValueError
 
@@ -144,9 +103,16 @@ def main():
         faster_rcnn.extractor.bn1.disable_update()
         faster_rcnn.extractor.res2.disable_update()
 
-    train_data = TransformDataset(train_data, Transform(faster_rcnn))
-    test_data = TransformDataset(test_data,
-                                 Transform(faster_rcnn, augmentation=False))
+    # Dataset
+    # -------------------------------------------------------------------------
+    train_data = FasterRCNNDataset(
+        mrcnn.datasets.VOC2012InstanceSeg(split='train'))
+    test_data = FasterRCNNDataset(
+        mrcnn.datasets.VOC2012InstanceSeg(split='val'))
+
+    train_data = TransformDataset(train_data, FasterRCNNTransform(faster_rcnn))
+    test_data = TransformDataset(
+        test_data, FasterRCNNTransform(faster_rcnn, augmentation=False))
 
     train_iter = chainer.iterators.MultiprocessIterator(
         train_data, batch_size=1, n_processes=None, shared_mem=100000000)
@@ -155,6 +121,8 @@ def main():
     updater = chainer.training.updater.StandardUpdater(
         train_iter, optimizer, device=args.gpu)
 
+    # Trainer
+    # -------------------------------------------------------------------------
     trainer = training.Trainer(
         updater, (args.iteration, 'iteration'), out=args.out)
 
