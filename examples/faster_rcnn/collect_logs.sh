32775d2fbf5dd62c5f7912967270b1807ef39263
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-24 00:59:47 +0900

    Add collect_logs.sh

---
+++ examples/faster_rcnn/collect_logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+set -x
+
+cd $HERE
+
+for server in hoop green dlbox1 dlbox2 dlbox3 dlbox4 dlbox5; do
+  echo $server
+  rsync -avt $server:mask-rcnn/experiments/faster_rcnn/logs .
+done
+
+set +x
