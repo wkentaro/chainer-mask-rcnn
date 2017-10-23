fb60ee87e1de6efdc40e00e67c0bee3a2c193b29
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-23 15:20:27 +0900

    Add evaluate.py

---
+++ examples/mask_rcnn/evaluate.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os.path as osp
+import pprint
+
+import chainer
+from chainercv.datasets import voc_bbox_label_names
+
+import mask_rcnn as mrcnn
+
+from train import InstanceSegmentationVOCEvaluator
+from train import TransformDataset
+
+
+gpu = 0
+if gpu >= 0:
+    chainer.cuda.get_device_from_id(gpu).use()
+chainer.global_config.train = False
+chainer.global_config.enable_backprop = False
+
+log_name = 'model=resnet50.pretrained_model=voc12_train_rpn.lr=0.001.seed=0.step_size=50000.iteration=70000.weight_decay=0.0005.update_policy=almost_all.pooling_func=pooling.overfit=False.timestamp=20171023_125628'
+# log_name = 'model=resnet50.pretrained_model=voc12_train_rpn.lr=0.001.seed=0.step_size=50000.iteration=70000.weight_decay=0.0005.update_policy=almost_all.pooling_func=align.overfit=False.timestamp=20171023_124734'
+pretrained_model = osp.join('logs', log_name, 'snapshot_model.npz')
+
+if 'model=resnet50.' in log_name:
+    model = 'resnet50'
+elif 'model=resnet101.' in log_name:
+    model = 'resnet101'
+else:
+    raise ValueError
+
+if '.pooling_func=align.' in log_name:
+    pooling_func = mrcnn.functions.roi_align_2d
+elif '.pooling_func=pooling.' in log_name:
+    pooling_func = chainer.functions.roi_pooling_2d
+else:
+    raise RuntimeError
+
+
+print('log_name:', log_name)
+print('model:', model)
+print('pooling_func:', pooling_func)
+
+
+n_layers = int(model.lstrip('resnet'))
+mask_rcnn = mrcnn.models.MaskRCNNResNet(
+    n_layers=n_layers,
+    n_fg_class=len(voc_bbox_label_names),
+    pretrained_model=pretrained_model,
+    pooling_func=pooling_func)
+if gpu >= 0:
+    mask_rcnn.to_gpu()
+
+
+def transform_test_data(in_data):
+    img = in_data[0]
+    img = img.transpose(2, 0, 1)
+    out_data = list(in_data)
+    out_data[0] = img
+    return tuple(out_data)
+
+
+test_data = mrcnn.datasets.MaskRcnnDataset(
+    mrcnn.datasets.VOC2012InstanceSeg('val'))
+test_data = TransformDataset(test_data, transform_test_data)
+test_iter = chainer.iterators.SerialIterator(
+    test_data, batch_size=1, repeat=False, shuffle=False)
+
+
+class DummyTrainer(object):
+
+    class DummyUpdater(object):
+
+        iteration = 'best'
+
+    updater = DummyUpdater()
+    out = 'logs/evaluate'
+
+
+evaluator = InstanceSegmentationVOCEvaluator(
+    test_iter, mask_rcnn, use_07_metric=False,
+    label_names=voc_bbox_label_names,
+    file_name='{}.iteration=%s.jpg'.format(log_name),
+)
+result = evaluator(trainer=DummyTrainer())
+pprint.pprint(result)
