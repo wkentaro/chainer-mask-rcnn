e39fd4129d350d398de951a1e37cbebe49868798
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-23 12:44:40 +0900

    Copy extractor's weight correctly

--- mask_rcnn/models/mask_rcnn_resnet.py
+++ mask_rcnn/models/mask_rcnn_resnet.py
@@ -155,8 +155,16 @@ class MaskRCNNResNet(MaskRCNN):
         pretrained_model = self._FasterRCNN(
             n_fg_class=n_fg_class, pretrained_model=pretrained_model)
 
-        self.extractor.copyparams(pretrained_model.extractor)
-        _copy_persistent_chain(self.extractor, pretrained_model.extractor)
+        self.extractor.conv1.copyparams(pretrained_model.extractor.conv1)
+        _copy_persistent_chain(self.extractor.conv1, pretrained_model.extractor.conv1)
+        self.extractor.bn1.copyparams(pretrained_model.extractor.bn1)
+        _copy_persistent_chain(self.extractor.bn1, pretrained_model.extractor.bn1)
+        self.extractor.res2.copyparams(pretrained_model.extractor.res2)
+        _copy_persistent_chain(self.extractor.res2, pretrained_model.extractor.res2)
+        self.extractor.res3.copyparams(pretrained_model.extractor.res3)
+        _copy_persistent_chain(self.extractor.res3, pretrained_model.extractor.res3)
+        self.extractor.res4.copyparams(pretrained_model.extractor.res4)
+        _copy_persistent_chain(self.extractor.res4, pretrained_model.extractor.res4)
 
         self.rpn.copyparams(pretrained_model.rpn)
         _copy_persistent_chain(self.rpn, pretrained_model.rpn)
