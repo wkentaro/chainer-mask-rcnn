10e6413901826b08d7fc4f9c0a56125f936f5f98
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-11-04 00:44:52 +0900

    Update summarize_logs.py and collect_logs.sh

--- experiments/mask_rcnn/collect_logs.sh
+++ experiments/mask_rcnn/collect_logs.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
-
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-set -x
-
-cd $HERE
+#!/bin/bash -x
 
 if [ "$(hostname)" != "hoop" ]; then
   exit 1
 fi
 
-for server in hoop green dlbox1 dlbox2 dlbox3 dlbox4 dlbox5 baxter-c1; do
-  [ "$server" = "hoop" ] && continue
-  timeout 1 ssh $server ls &>/dev/null || continue
-  rsync -avt $server:mask-rcnn/experiments/mask_rcnn/logs/ logs/
-done
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $HERE
 
-set +x
+for server in green dlbox1 dlbox2 dlbox3 dlbox4 dlbox5 baxter-c1; do
+  timeout 1 ssh $server ls &>/dev/null &&
+    rsync -avt $server:mask-rcnn/experiments/mask_rcnn/logs/ logs/ &
+done
+wait
