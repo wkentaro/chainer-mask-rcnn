56f7b6963f64dcd0eca06a10bf3462a67733f661
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-01-20 00:27:00 +0900

    Update experiments/mask_rcnn/collect_logs.sh

--- examples/mask_rcnn/collect_logs.sh
+++ examples/mask_rcnn/collect_logs.sh
@@ -9,6 +9,6 @@ cd $HERE
 
 for server in green dlbox1 dlbox2 dlbox3 dlbox4 dlbox5 baxter-c1; do
   timeout 1 ssh $server ls &>/dev/null &&
-    rsync -avt $server:mask-rcnn/experiments/mask_rcnn/logs/ logs/ &
+    rsync -avt $server:chainer-mask-rcnn/experiments/mask_rcnn/logs/ logs/ &
 done
 wait
