01fd68927fd83718fa3ce13110c71fccfaea72da
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-01-20 00:50:38 +0900

    Update data scripts

--- experiments/mask_rcnn/collect_logs.sh
+++ experiments/mask_rcnn/collect_logs.sh
@@ -7,7 +7,7 @@ fi
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $HERE
 
-for server in green dlboxs1 dlbox1 dlbox3 dlbox4 dlbox5; do
+for server in dlboxs1 dlbox1 dlbox3 dlbox4 dlbox5 crux; do
   timeout 1 ssh $server ls &>/dev/null &&
     rsync -avt $server:chainer-mask-rcnn/experiments/mask_rcnn/logs/ logs/ &
 done
--- experiments/mask_rcnn/setup_datasets.sh
+++ experiments/mask_rcnn/setup_datasets.sh
@@ -10,8 +10,7 @@ if [ "$(hostname)" != "hoop" ]; then
   exit 1
 fi
 
-for server in hoop green dlbox1 dlbox2 dlbox3 dlbox4 dlbox5; do
-  [ "$server" = "hoop" ] && continue
+for server in dlboxs1 dlbox1 dlbox3 dlbox4 dlbox5 crux; do
   timeout 1 ssh $server ls &>/dev/null || continue
   rsync -avt --progress ~/data/datasets/pfnet/ $server:data/datasets/pfnet/
   rsync -avt --progress ~/data/datasets/VOC/ $server:data/datasets/VOC/
