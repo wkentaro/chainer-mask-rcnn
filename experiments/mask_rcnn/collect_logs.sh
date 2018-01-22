67b92151ced683eee2e656e0cbfd4da411454fa8
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-01-22 21:18:33 +0900

    Update collect_logs.sh

--- experiments/mask_rcnn/collect_logs.sh
+++ experiments/mask_rcnn/collect_logs.sh
@@ -7,7 +7,7 @@ fi
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $HERE
 
-for server in dlboxs1 dlbox1 dlbox3 dlbox4 dlbox5 crux; do
+for server in dlboxs1 dlbox1 dlbox4 dlbox5; do
   timeout 1 ssh $server ls &>/dev/null &&
     rsync -avt $server:chainer-mask-rcnn/experiments/mask_rcnn/logs/ logs/ &
 done
