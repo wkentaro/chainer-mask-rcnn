9f051194d49534a826067efde6cf7bf1ae658f62
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-02-03 03:07:29 +0900

    Add summarize_logs.py for coco

---
+++ examples/coco/summarize_logs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os.path as osp
+import sys
+
+here = osp.dirname(osp.abspath(__file__))
+sys.path.insert(0, osp.join(here, '../voc'))
+from summarize_logs import summarize_logs
+
+
+if __name__ == '__main__':
+    keys = [
+        'name',
+        # 'timestamp',
+        'last_time',
+        # 'dataset',
+        'git_hash',
+        # 'git_branch',
+        'hostname',
+        'model',
+        'lr', 'pooling_func',
+        'epoch', 'iteration',
+        # 'main/loss',
+        'validation/main/map',
+    ]
+    objective = 'max'
+    summarize_logs('logs', keys, target_key=keys[-1], objective=objective)
