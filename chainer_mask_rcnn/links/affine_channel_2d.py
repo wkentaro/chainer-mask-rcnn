a882d80662afd07723afd90b80661c588bb863b5
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-20 00:02:28 +0900

    Refactor AffineChannel2D

--- chainer_mask_rcnn/links/affine_channel_2d.py
+++ chainer_mask_rcnn/links/affine_channel_2d.py
@@ -10,18 +10,14 @@ class AffineChannel2D(chainer.Link):
     def __init__(self, channels):
         super(AffineChannel2D, self).__init__()
         with self.init_scope():
-            W_initializer = initializers.One()
-            self.W = chainer.variable.Parameter(W_initializer, (channels,))
-            b_initializer = initializers.Zero()
-            self.b = chainer.variable.Parameter(b_initializer, (channels,))
+            self.W = chainer.variable.Parameter(
+                initializers.One(), (channels,))
+            self.b = chainer.variable.Parameter(
+                initializers.Zero(), (channels,))
 
     def __call__(self, x):
         W = functions.reshape(self.W, (1, -1, 1, 1))
         W = functions.broadcast_to(W, x.shape)
-        y = x * W
-        del W
         b = functions.reshape(self.b, (1, -1, 1, 1))
         b = functions.broadcast_to(b, x.shape)
-        y += b
-        del b
-        return y
+        return W * x + b
