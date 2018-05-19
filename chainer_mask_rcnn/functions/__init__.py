fef08b76d8b7230326d75a5555f8128a9ce3992a
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-20 02:51:51 +0900

    Test AffineChannel2DFunction

--- chainer_mask_rcnn/functions/__init__.py
+++ chainer_mask_rcnn/functions/__init__.py
@@ -1,4 +1,8 @@
 # flake8: noqa
+
+from .affine_channel_2d import affine_channel_2d
+from .affine_channel_2d import AffineChannel2DFunction
+
 from .roi_align_2d import crop_and_resize
 from .roi_align_2d import roi_align_2d
 from .roi_align_2d import ROIAlign2D
---
+++ chainer_mask_rcnn/functions/affine_channel_2d.py
@@ -0,0 +1,53 @@
+import chainer
+from chainer import cuda
+from chainer import functions
+from chainer.utils import type_check
+import numpy
+
+
+class AffineChannel2DFunction(chainer.Function):
+
+    def forward(self, inputs):
+        self.retain_inputs((0, 1, 2))
+        xp = cuda.get_array_module(inputs)
+        x, W, b = inputs
+        if xp is numpy:
+            y = W * x + b
+        else:
+            y = cuda.elementwise(
+                'T x, T W, T b', 'T y',
+                'y = W * x + b', 'affine_fwd'
+            )(x, W, b)
+        return y,
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 3)
+
+    def backward(self, inputs, gy):
+        xp = cuda.get_array_module(inputs)
+        x, W, b = inputs
+        gy, = gy
+
+        if xp is numpy:
+            gx = W * gy
+            gW = x * gy
+        else:
+            gx, gW = cuda.elementwise(
+                'T x, T W, T b, T gy', 'T gx, T gW',
+                'gx = W * gy; gW = x * gy;', 'affine_bwd'
+            )(x, W, b, gy)
+        gb = gy
+
+        gW = xp.sum(gW, axis=(0, 2, 3), keepdims=True)
+        gb = xp.sum(gb, axis=(0, 2, 3), keepdims=True)
+        return gx, gW, gb
+
+
+def affine_channel_2d_naive(x, W, b):
+    W = functions.broadcast_to(W, x.shape)
+    b = functions.broadcast_to(b, x.shape)
+    return W * x + b
+
+
+def affine_channel_2d(x, W, b):
+    return AffineChannel2DFunction()(x, W, b)
