3ed38a76a2bc3c9a6c4fc834b40d882f29fe9853
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-22 01:27:03 +0900

    Update AffineChannel2D also

--- examples/coco/train.py
+++ examples/coco/train.py
@@ -142,9 +142,6 @@ def main():
         mask_rcnn.extractor.conv1.disable_update()
         mask_rcnn.extractor.bn1.disable_update()
         mask_rcnn.extractor.res2.disable_update()
-        for link in mask_rcnn.links():
-            if isinstance(link, mrcnn.links.AffineChannel2D):
-                link.disable_update()
 
     train_data = chainer.datasets.TransformDataset(
         train_data, mrcnn.datasets.MaskRCNNTransform(mask_rcnn))
