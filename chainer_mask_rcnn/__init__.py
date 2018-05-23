5407042cd95b5ba6e83bc3b0ca7f2ab125741dfc
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-23 23:33:35 +0900

    Add chainer_mask_rcnn.__version__

--- chainer_mask_rcnn/__init__.py
+++ chainer_mask_rcnn/__init__.py
@@ -1,4 +1,11 @@
 # flake8: noqa
+
+import pkg_resources
+
+
+__version__ = pkg_resources.get_distribution('chainer_mask_rcnn').version
+
+
 from . import datasets
 from . import extensions
 from . import functions
