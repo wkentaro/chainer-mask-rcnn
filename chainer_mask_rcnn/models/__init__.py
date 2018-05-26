7c05f23c423326755b361906eadeef9df078cbda
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-26 15:16:11 +0900

    Drop rpn_train_chain.py

--- chainer_mask_rcnn/models/__init__.py
+++ chainer_mask_rcnn/models/__init__.py
@@ -2,10 +2,8 @@
 
 from . import utils
 
-from .faster_rcnn_resnet import FasterRCNNResNet
 from .mask_rcnn import MaskRCNN
 from .mask_rcnn_resnet import MaskRCNNResNet
 from .mask_rcnn_train_chain import MaskRCNNTrainChain
 from .mask_rcnn_vgg import MaskRCNNVGG16
 from .mask_rcnn_vgg import VGG16RoIHead
-from .rpn_train_chain import RPNTrainChain
