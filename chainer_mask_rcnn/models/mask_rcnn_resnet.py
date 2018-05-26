ef4f80ae13c30cc9d47dffdc9f3a11613e4539cc
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-26 16:19:20 +0900

    Add ResNetExtractor

--- chainer_mask_rcnn/models/mask_rcnn_resnet.py
+++ chainer_mask_rcnn/models/mask_rcnn_resnet.py
@@ -11,8 +11,6 @@
 # https://github.com/chainer/chainercv
 # --------------------------------------------------------
 
-import collections
-
 import numpy as np
 
 import chainer
@@ -20,125 +18,14 @@ import chainer.functions as F
 import chainer.links as L
 
 from chainer.links.model.vision.resnet import BuildingBlock
-from chainer.links.model.vision.resnet import ResNet101Layers
-from chainer.links.model.vision.resnet import ResNet50Layers
 from chainercv.utils import download_model
 
 from .. import functions
-from .. import links
 from .mask_rcnn import MaskRCNN
 from .region_proposal_network import RegionProposalNetwork
-
-
-def _get_affine_from_bn(bn):
-    channels = bn.gamma.size
-    bn_mean = bn.avg_mean
-    bn_var = bn.avg_var
-    scale = bn.gamma.data
-    bias = bn.beta.data
-    xp = chainer.cuda.get_array_module(bn_var)
-    std = xp.sqrt(bn_var + 1e-5)
-    new_scale = scale / std
-    new_bias = bias - bn_mean * new_scale
-    affine = links.AffineChannel2D(channels)
-    affine.W.data[:] = new_scale[:]
-    affine.b.data[:] = new_bias[:]
-    return affine
-
-
-def _convert_bn_to_affine(chain):
-    for name, link in chain.namedlinks():
-        if not isinstance(link, L.BatchNormalization):
-            continue
-        for key in name.split('/')[:-1]:
-            if key == '':
-                parent = chain
-            else:
-                parent = getattr(parent, key)
-        key = name.split('/')[-1]
-        delattr(parent, key)
-        link2 = _get_affine_from_bn(link)
-        parent.add_link(key, link2)
-
-
-class ResNet50Extractor(ResNet50Layers):
-
-    mode = 'all'
-
-    def __init__(self, *args, **kwargs):
-        remove_layers = kwargs.pop('remove_layers', True)
-        super(ResNet50Extractor, self).__init__(*args, **kwargs)
-        if remove_layers:
-            # Remove no need layers to save memory
-            delattr(self, 'res5')
-            delattr(self, 'fc6')
-        _convert_bn_to_affine(self)
-
-    @property
-    def functions(self):
-        return collections.OrderedDict([
-            ('conv1', [self.conv1, self.bn1, F.relu]),
-            ('pool1', [lambda x: F.max_pooling_2d(x, 3, stride=2, pad=1)]),
-            ('res2', [self.res2]),
-            ('res3', [self.res3]),
-            ('res4', [self.res4]),
-        ])
-
-    def __call__(self, x):
-        assert self.mode in ['head', 'res3+', 'res4+', 'all']
-        h = x
-        for key, funcs in self.functions.items():
-            for func in funcs:
-                h = func(h)
-            if key == 'res2' and self.mode == 'res3+':
-                h.unchain_backward()
-            if key == 'res3' and self.mode == 'res4+':
-                h.unchain_backward()
-            if key == 'res4':
-                if self.mode == 'head':
-                    h.unchain_backward()
-                break
-        return h
-
-
-class ResNet101Extractor(ResNet101Layers):
-
-    mode = 'all'
-
-    def __init__(self, *args, **kwargs):
-        remove_layers = kwargs.pop('remove_layers', True)
-        super(ResNet101Extractor, self).__init__(*args, **kwargs)
-        if remove_layers:
-            # Remove no need layers to save memory
-            delattr(self, 'res5')
-            delattr(self, 'fc6')
-        _convert_bn_to_affine(self)
-
-    @property
-    def functions(self):
-        return collections.OrderedDict([
-            ('conv1', [self.conv1, self.bn1, F.relu]),
-            ('pool1', [lambda x: F.max_pooling_2d(x, ksize=3, stride=2)]),
-            ('res2', [self.res2]),
-            ('res3', [self.res3]),
-            ('res4', [self.res4]),
-        ])
-
-    def __call__(self, x):
-        assert self.mode in ['head', 'res3+', 'res4+', 'all']
-        h = x
-        for key, funcs in self.functions.items():
-            for func in funcs:
-                h = func(h)
-            if key == 'res2' and self.mode == 'res3+':
-                h.unchain_backward()
-            if key == 'res3' and self.mode == 'res4+':
-                h.unchain_backward()
-            if key == 'res4':
-                if self.mode == 'head':
-                    h.unchain_backward()
-                break
-        return h
+from .resnet_extractor import _convert_bn_to_affine
+from .resnet_extractor import ResNet101Extractor
+from .resnet_extractor import ResNet50Extractor
 
 
 class MaskRCNNResNet(MaskRCNN):
---
+++ chainer_mask_rcnn/models/resnet_extractor.py
@@ -0,0 +1,94 @@
+import collections
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer.links.model.vision.resnet import ResNet50Layers
+from chainer.links.model.vision.resnet import ResNet101Layers
+
+from .. import links
+
+
+def _get_affine_from_bn(bn):
+    channels = bn.gamma.size
+    bn_mean = bn.avg_mean
+    bn_var = bn.avg_var
+    scale = bn.gamma.data
+    bias = bn.beta.data
+    xp = chainer.cuda.get_array_module(bn_var)
+    std = xp.sqrt(bn_var + 1e-5)
+    new_scale = scale / std
+    new_bias = bias - bn_mean * new_scale
+    affine = links.AffineChannel2D(channels)
+    affine.W.data[:] = new_scale[:]
+    affine.b.data[:] = new_bias[:]
+    return affine
+
+
+def _convert_bn_to_affine(chain):
+    for name, link in chain.namedlinks():
+        if not isinstance(link, L.BatchNormalization):
+            continue
+        for key in name.split('/')[:-1]:
+            if key == '':
+                parent = chain
+            else:
+                parent = getattr(parent, key)
+        key = name.split('/')[-1]
+        delattr(parent, key)
+        link2 = _get_affine_from_bn(link)
+        parent.add_link(key, link2)
+
+
+class ResNetExtractorBase(object):
+
+    mode = 'all'
+
+    def _init_layers(self, remove_layers):
+        if remove_layers:
+            # Remove no need layers to save memory
+            delattr(self, 'res5')
+            delattr(self, 'fc6')
+        _convert_bn_to_affine(self)
+
+    @property
+    def functions(self):
+        return collections.OrderedDict([
+            ('conv1', [self.conv1, self.bn1, F.relu]),
+            ('pool1', [lambda x: F.max_pooling_2d(x, 3, stride=2, pad=1)]),
+            ('res2', [self.res2]),
+            ('res3', [self.res3]),
+            ('res4', [self.res4]),
+        ])
+
+    def __call__(self, x):
+        assert self.mode in ['head', 'res3+', 'res4+', 'all']
+        h = x
+        for key, funcs in self.functions.items():
+            for func in funcs:
+                h = func(h)
+            if key == 'res2' and self.mode == 'res3+':
+                h.unchain_backward()
+            if key == 'res3' and self.mode == 'res4+':
+                h.unchain_backward()
+            if key == 'res4':
+                if self.mode == 'head':
+                    h.unchain_backward()
+                break
+        return h
+
+
+class ResNet50Extractor(ResNetExtractorBase, ResNet50Layers):
+
+    def __init__(self, *args, **kwargs):
+        remove_layers = kwargs.pop('remove_layers', True)
+        super(ResNet50Extractor, self).__init__(*args, **kwargs)
+        self._init_layers(remove_layers)
+
+
+class ResNet101Extractor(ResNetExtractorBase, ResNet101Layers):
+
+    def __init__(self, *args, **kwargs):
+        remove_layers = kwargs.pop('remove_layers', True)
+        super(ResNet101Extractor, self).__init__(*args, **kwargs)
+        self._init_layers(remove_layers)
