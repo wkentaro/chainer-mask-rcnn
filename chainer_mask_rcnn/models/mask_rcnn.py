303c6345cd44c412c340529c821b14cb48a18368
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-07-29 06:54:26 +0900

    Remove documentation

--- chainer_mask_rcnn/models/mask_rcnn.py
+++ chainer_mask_rcnn/models/mask_rcnn.py
@@ -102,59 +102,6 @@ def segm_results(bbox, label, roi_mask, im_h, im_w, mask_size):
 
 class MaskRCNN(chainer.Chain):
 
-    """Base class for Faster R-CNN.
-
-    This is a base class for Faster R-CNN links supporting object detection
-    API [#]_. The following three stages constitute Faster R-CNN.
-
-    1. **Feature extraction**: Images are taken and their \
-        feature maps are calculated.
-    2. **Region Proposal Networks**: Given the feature maps calculated in \
-        the previous stage, produce set of RoIs around objects.
-    3. **Localization and Classification Heads**: Using feature maps that \
-        belong to the proposed RoIs, classify the categories of the objects \
-        in the RoIs and improve localizations.
-
-    Each stage is carried out by one of the callable
-    :class:`chainer.Chain` objects :obj:`feature`, :obj:`rpn` and :obj:`head`.
-
-    There are two functions :meth:`predict` and :meth:`__call__` to conduct
-    object detection.
-    :meth:`predict` takes images and returns bounding boxes that are converted
-    to image coordinates. This will be useful for a scenario when
-    Faster R-CNN is treated as a black box function, for instance.
-    :meth:`__call__` is provided for a scnerario when intermediate outputs
-    are needed, for instance, for training and debugging.
-
-    Links that support obejct detection API have method :meth:`predict` with
-    the same interface. Please refer to :func:`FasterRCNN.predict` for
-    further details.
-
-    .. [#] Shaoqing Ren, Kaiming He, Ross Girshick, Jian Sun. \
-    Faster R-CNN: Towards Real-Time Object Detection with \
-    Region Proposal Networks. NIPS 2015.
-
-    Args:
-        extractor (callable Chain): A callable that takes a BCHW image
-            array and returns feature maps.
-        rpn (callable Chain): A callable that has the same interface as
-            :class:`chainercv.links.RegionProposalNetwork`. Please refer to
-            the documentation found there.
-        head (callable Chain): A callable that takes
-            a BCHW array, RoIs and batch indices for RoIs. This returns class
-            dependent localization paramters and class scores.
-        mean (numpy.ndarray): A value to be subtracted from an image
-            in :meth:`prepare`.
-        min_size (int): A preprocessing paramter for :meth:`prepare`. Please
-            refer to a docstring found for :meth:`prepare`.
-        max_size (int): A preprocessing paramter for :meth:`prepare`.
-        loc_normalize_mean (tuple of four floats): Mean values of
-            localization estimates.
-        loc_normalize_std (tupler of four floats): Standard deviation
-            of localization estimates.
-
-    """
-
     def __init__(
             self, extractor, rpn, head,
             mean,
@@ -186,42 +133,6 @@ class MaskRCNN(chainer.Chain):
         return self.head.n_class
 
     def __call__(self, x, scales):
-        """Forward Faster R-CNN.
-
-        Scaling paramter :obj:`scale` is used by RPN to determine the
-        threshold to select small objects, which are going to be
-        rejected irrespective of their confidence scores.
-
-        Here are notations used.
-
-        * :math:`N` is the number of batch size
-        * :math:`R'` is the total number of RoIs produced across batches. \
-            Given :math:`R_i` proposed RoIs from the :math:`i` th image, \
-            :math:`R' = \\sum _{i=1} ^ N R_i`.
-        * :math:`L` is the number of classes excluding the background.
-
-        Classes are ordered by the background, the first class, ..., and
-        the :math:`L` th class.
-
-        Args:
-            x (~chainer.Variable): 4D image variable.
-            scale (float): Amount of scaling applied to the raw image
-                during preprocessing.
-
-        Returns:
-            Variable, Variable, array, array:
-            Returns tuple of four values listed below.
-
-            * **roi_cls_locs**: Offsets and scalings for the proposed RoIs. \
-                Its shape is :math:`(R', (L + 1) \\times 4)`.
-            * **roi_scores**: Class predictions for the proposed RoIs. \
-                Its shape is :math:`(R', L + 1)`.
-            * **rois**: RoIs proposed by RPN. Its shape is \
-                :math:`(R', 4)`.
-            * **roi_indices**: Batch indices of RoIs. Its shape is \
-                :math:`(R',)`.
-
-        """
         img_size = x.shape[2:]
 
         h = self.extractor(x)
@@ -232,25 +143,6 @@ class MaskRCNN(chainer.Chain):
         return roi_cls_locs, roi_scores, rois, roi_indices, roi_masks
 
     def prepare(self, img):
-        """Preprocess an image for feature extraction.
-
-        The length of the shorter edge is scaled to :obj:`self.min_size`.
-        After the scaling, if the length of the longer edge is longer than
-        :obj:`self.max_size`, the image is scaled to fit the longer edge
-        to :obj:`self.max_size`.
-
-        After resizing the image, the image is subtracted by a mean image value
-        :obj:`self.mean`.
-
-        Args:
-            img (~numpy.ndarray): An image. This is in CHW and RGB format.
-                The range of its value is :math:`[0, 255]`.
-
-        Returns:
-            ~numpy.ndarray:
-            A preprocessed image.
-
-        """
         _, H, W = img.shape
 
         scale = 1.
