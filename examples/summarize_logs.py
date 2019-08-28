fcf5ebb4fb8bb87d6fc8e27368d7f5ae0d4a26df
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-28 13:32:00 +0100

    Fix for flake8

--- examples/summarize_logs.py
+++ examples/summarize_logs.py
@@ -115,7 +115,7 @@ def summarize_log(logs_dir, name, keys, target_key, objective):
             else:
                 elapsed_time = df['elapsed_time'].max()
                 value = params.get('timestamp', '<none>')
-                if value is not '<none>':
+                if value != '<none>':
                     value = dateutil.parser.parse(value)
                     value += datetime.timedelta(seconds=elapsed_time)
                     now = datetime.datetime.now()
